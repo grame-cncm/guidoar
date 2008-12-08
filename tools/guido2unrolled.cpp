d1337d2e6ff02bceb89d98d980bbfcb65aa5fa4e
fober <fober@2a570e91-86af-4e14-8b94-b0b5b3996bc3>
2008-12-08 15:17:16 +0000

    guido2unrolled tool added
    git-svn-id: svn+ssh://nina.grame.fr/home/svn/guidoar@119 2a570e91-86af-4e14-8b94-b0b5b3996bc3

---
+++ tools/guido2unrolled.cpp
@@ -0,0 +1,77 @@
+/*
+
+  Copyright (C) 2003-2008  Grame
+  Grame Research Laboratory, 9 rue du Garet, 69001 Lyon - France
+  research@grame.fr
+
+  This file is provided as an example of the MusicXML Library use.
+*/
+
+#ifdef WIN32
+# pragma warning (disable : 4786)
+# define basename(name)	(name)
+# define _CRT_SECURE_NO_DEPRECATE
+#else 
+# include <libgen.h>
+#endif
+
+#include <stdlib.h>
+#include <iostream>
+
+#include "clonevisitor.h"
+#include "guidoparser.h"
+#include "unrolled_guido_browser.h"
+
+
+#define debug
+
+using namespace std;
+using namespace guido;
+
+
+//_______________________________________________________________________________
+static void usage(char * name)
+{
+	cerr << "usage: " << basename(name) << " <score>"  << endl;
+	cerr << "       generates an unrolled version of a score"  << endl;
+	cerr << "       score: the input file or '-' to read stdin"  << endl;
+	exit (1);
+}
+
+
+//_______________________________________________________________________________
+static SARMusic read (const char* file) 
+{
+	guidoparser r;
+	SARMusic score;
+	if (!strcmp(file, "-"))
+		score = r.parseFile(stdin);
+	else
+		score = r.parseFile(file);
+	if (!score) {
+		cerr << file << ": read failed"  << endl;
+		exit (1);
+	}
+	return score;
+}
+
+//_______________________________________________________________________________
+int main(int argc, char *argv[]) {
+#ifdef debug
+	char * file = "a.gmn";
+#else
+	if (argc != 2) usage(argv[0]);
+	char * file = argv[1];
+#endif
+
+	Sguidoelement score = read (file);
+	if (score) {
+		clonevisitor cv;
+		unrolled_guido_browser ugb(&cv);
+		ugb.browse (score);
+//		Sguidoelement unrolled = cv.clone(score, true);
+		Sguidoelement unrolled = cv.result();
+		if (unrolled) cout << unrolled << endl;
+	}
+	return 0;
+}
