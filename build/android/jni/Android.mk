f26f0f4f213ccbd1d25320fcf47efd801c5369e3
D. Fober <fober@grame.fr>
2015-12-23 11:29:59 +0100

    android makefile fixed

--- build/android/jni/Android.mk
+++ build/android/jni/Android.mk
@@ -9,10 +9,10 @@ LOCAL_MODULE := guidoar
 
 # SRC var is a trick to use wildcard in android.mk
 # if no wildcard, you have better to use relative path, conforming to android doc 
-LOCAL_SRC_FILES         := $(subst $(LOCAL_PATH)/,,$(wildcard $(SRC)/guido/*.cpp) $(wildcard $(SRC)/guido/abstract/*.cpp) $(wildcard $(SRC)/lib/*.cpp) $(wildcard $(SRC)/operations/*.cpp) $(wildcard $(SRC)/visitors/*.cpp) $(wildcard $(SRC)/midi/*.cpp) $(wildcard $(SRC)/parser/*.cpp) )
-LOCAL_EXPORT_C_INCLUDES := $(addprefix $(SRC)/, include devices lib)
+LOCAL_SRC_FILES         := $(subst $(LOCAL_PATH)/,,$(wildcard $(SRC)/guido/*.cpp) $(wildcard $(SRC)/guido/abstract/*.cpp) $(wildcard $(SRC)/lib/*.cpp) $(wildcard $(SRC)/operations/*.cpp) $(wildcard $(SRC)/visitors/*.cpp) $(wildcard $(SRC)/parser/*.cpp) $(wildcard $(SRC)/interface/*.cpp) )
+LOCAL_EXPORT_C_INCLUDES := $(addprefix $(SRC)/, interface)
 LOCAL_C_INCLUDES        := $(subst $(LOCAL_PATH)/../,,$(wildcard $(SRC)/*/) $(SRC)/guido/abstract)
-LOCAL_CPPFLAGS          := -frtti
+LOCAL_CPPFLAGS          := -DGUIDOAR_EXPORTS -frtti
 
 include $(BUILD_SHARED_LIBRARY)
 
